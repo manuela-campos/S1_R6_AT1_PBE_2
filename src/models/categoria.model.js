88743378c765fd2a2bfa633d320ce6b0780cf9c2
manuela-campos <manoelacampos1@gmail.com>
2026-06-18 10:39:25 -0300

    Atualização da model

--- src/models/categoria.model.js
+++ src/models/categoria.model.js
@@ -3,13 +3,13 @@ import { connection as pool } from "../config/db.js";
 const categoriaModel = {
   insertCategoria: async (cCategoria) => {
     const sql =
-      "INSERT INTO categoria (descricaoCategoria,dataCad) VALUES (?, NOW());";
+      "INSERT INTO categorias (descricaoCategoria, dataCad) VALUES (?, NOW());";
     const values = [cCategoria.descricaoCategoria, cCategoria.dataCad];
     const [rows] = await pool.query(sql, values);
     return rows;
   },
   selectCategoria: async () => {
-    const sql = 'SELECT * FROM categoria';
+    const sql = 'SELECT * FROM categorias';
     const [rows] = await pool.query(sql);
     return rows;
   },
@@ -17,33 +17,33 @@ const categoriaModel = {
         const sql = `
             SELECT 
                 c.id_categoria,
-                c.descricaoCategoria,
+                c.nome_categoria,
                 c.dataCad
-            FROM categoria
+            FROM categorias
             WHERE c.id_categoria = ?
         `;
         const [rows] = await pool.query(sql, [id]);
         return rows;
     },
     // Atuallizar o valor que deseja na tabela
-    update: async (id, descricaoCategoria) => {
+    update: async (id, nome_categoria) => {
         const sql = `
             UPDATE categoria 
             SET 
                 id_categoria = ?,
-                descricaoCategoria = ?
+                nome_categoria = ?
             WHERE id_categoria = ?
         `;
         // COALESCE mantém imagem antiga se não enviar nova
         const values = [
             id,
-            descricaoCategoria
+            nome_categoria
         ];
         const [rows] = await pool.query(sql, values);
         return rows;
       },
     delete: async () => {
-        const sql = 'DELETE FROM categoria WHERE id_categoria;';
+        const sql = 'DELETE FROM categorias WHERE id_categoria;';
         const [rows] = await pool.query(sql);
         return rows;
     },
